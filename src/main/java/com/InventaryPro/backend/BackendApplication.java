9f49b01a06e699dea38f3f573c9292d8bd0d8e3a
VOLIPI JAYAKRISHNA <jayakrishna5242@gmail.com>
2026-03-19 19:14:25 +0530

    Env added

--- src/main/java/com/InventaryPro/backend/BackendApplication.java
+++ src/main/java/com/InventaryPro/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.InventaryPro.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,9 +10,24 @@ import java.util.Map;
 
 @SpringBootApplication
 public class BackendApplication {
-
+ private static void setIfPresent(Dotenv dotenv, String key) {
+        String value = dotenv.get(key);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
+    }
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        setIfPresent(dotenv, "DB_URL");
+        setIfPresent(dotenv, "DB_USERNAME");
+        setIfPresent(dotenv, "DB_PASSWORD");
+
+
+        SpringApplication.run(BackendApplication.class, args);
 	}
 
 
